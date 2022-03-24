d709d8ae743835448d97433cc1fe3eb597458867
Oryto Mikli <orytomikli21@gmail.com>
2022-03-24 14:06:13 +0700

    Fix Date Error and add Reg Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cinematix/page/register.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
---
+++ lib/page/register.dart
@@ -0,0 +1,66 @@
+import 'package:cinematix/page/login.dart';
+import 'package:flutter/material.dart';
+
+// import 'package:cinematix/widget/cinematix_container.dart';
+import '../widget/cinematix_container.dart';
+
+class RegisterPage extends StatelessWidget {
+  const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: CinematixContainer(
+      formChild: [
+        TextField(
+          decoration: InputDecoration(
+            hintText: 'username',
+          ),
+        ),
+        TextField(
+          decoration: InputDecoration(
+            hintText: 'nama lengkap',
+          ),
+        ),
+        TextField(
+          decoration: InputDecoration(
+            hintText: 'email',
+          ),
+        ),
+        TextField(
+          decoration: InputDecoration(
+            hintText: 'no hp',
+          ),
+        ),
+        TextField(
+          obscureText: true,
+          autocorrect: false,
+          enableSuggestions: false,
+          decoration: InputDecoration(
+            hintText: 'password',
+          ),
+        ),
+        SizedBox(
+          width: 200,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text("Register"),
+          ),
+        )
+      ],
+      bottomChild: [
+        SizedBox(
+          width: 200,
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => LoginPage()));
+            },
+            child: const Text("Login"),
+          ),
+        ),
+        const Text("Sudah Punya Akun ?")
+      ],
+    ));
+  }
+}
