cd87e596887c121e2e55fb663f40814f3189ffe4
ria0901 <80446413+ria0901@users.noreply.github.com>
2022-03-24 07:35:44 +0700

    Update movie.dart

--- lib/core/movie.dart
+++ lib/core/movie.dart
@@ -3,30 +3,50 @@ class Movie {
   final String __image;
   final String __pv;
   final String __description;
-  final double __rating;
+  final String __rating;
+  final int __happiness;
+  final int __lenght;
+  final Date __start_airing;
+  final Date __end_airing;
 
   Movie(
       {required String title,
       required String image,
       String? pv,
       String? description,
-      double? rating})
+      String? rating, 
+      int? happiness,
+      int? lenght,
+      Date? start_airing,
+      Date? end_airing})
       : __title = title,
         __image = image,
         __pv = pv!,
         __description = description!,
-        __rating = rating!;
+        __rating = rating!,
+        __happiness = happiness!,
+        __lenght = lenght!,
+        __start_airing = start_airing!,
+        __end_airing = end_airing!;
 
   factory Movie.fromJSON(Map<String, dynamic> json) => Movie(
       title: json['title'] as String,
       image: json['title'] as String,
       pv: json['pv'] as String,
       description: json['description'] as String,
-      rating: json['rating'] as double);
+      rating: json['rating'] as String,
+      happiness: json['happiness'] as int,
+      lenght: json['length'] as int
+      start_airing: json['start_airing'] as Date,
+      end_airing: json['end_airing'] as Date);
 
   String getTitle() => __title;
   String getImage() => __image;
   String getPV() => __pv;
   String getDescription() => __description;
-  double getRating() => __rating;
+  String getRating() => __rating;
+  int getHappiness() => __happiness;
+  int getLenght() => __lenght;
+  Date getStart_airing() => __start_airing;
+  Date getEnd_airing() => __end_airing;
 }
