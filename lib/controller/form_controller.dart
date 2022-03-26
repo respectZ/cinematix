72860737df4a8aa28ffcec95f66914828a884eff
Svdex <ucihaibna@yahoo.com>
2022-03-26 10:02:23 +0700

    Dummy text controller

---
+++ lib/controller/form_controller.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class FormController {
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+}
