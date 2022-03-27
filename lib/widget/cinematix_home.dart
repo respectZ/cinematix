6075d7d35aa366142841cbf553f7676e221bc0ae
Svdex <ucihaibna@yahoo.com>
2022-03-27 14:48:14 +0700

    Move to cinematix bar

--- lib/widget/cinematix_home.dart
+++ lib/widget/cinematix_home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'cinematix_bar.dart';
 
 class CinematixHome extends StatefulWidget {
   List<Widget> filmList = [];
@@ -10,111 +11,45 @@ class CinematixHome extends StatefulWidget {
 }
 
 class _CinematixHomeState extends State<CinematixHome> {
-  int _currentIndex = 0;
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.w500);
-  final List<Widget> _children = [
-    const Text(
-      "Home",
-      style: optionStyle,
-    ),
-    const Text(
-      "Qrcode",
-      style: optionStyle,
-    ),
-    const Text(
-      "Ticket",
-      style: optionStyle,
-    ),
-  ];
-
-  void onTabTapped(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          title: const Text(
-            "CINEMATIX",
-            style: TextStyle(color: Color.fromARGB(255, 0, 166, 232)),
-          ),
-          centerTitle: false,
-          actions: [
-            TextButton(
-                onPressed: () {},
-                child: const Text(
-                  "Artline660",
-                  style: TextStyle(color: Colors.black87),
-                )),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.person_outline,
-                color: Colors.black87,
-              ),
-              iconSize: 20,
-            )
-          ],
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: const Text(
+          "CINEMATIX",
+          style: TextStyle(color: Color.fromARGB(255, 0, 166, 232)),
         ),
-        body: Stack(children: [
-          Container(
-            child: Row(
-              children: widget.filmList,
+        centerTitle: false,
+        actions: [
+          TextButton(
+              onPressed: () {},
+              child: const Text(
+                "Artline660",
+                style: TextStyle(color: Colors.black87),
+              )),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.person_outline,
+              color: Colors.black87,
             ),
+            iconSize: 20,
           )
-        ]),
-        bottomNavigationBar: Container(
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(20), topLeft: Radius.circular(20)),
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.black38, spreadRadius: 0, blurRadius: 10),
-              ],
-            ),
-            child: ClipRRect(
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(20.0),
-                  topRight: Radius.circular(20.0),
-                ),
-                child: BottomNavigationBar(
-                    type: BottomNavigationBarType.fixed,
-                    selectedItemColor: Colors.white,
-                    unselectedItemColor: Colors.grey[300],
-                    currentIndex: _currentIndex,
-                    onTap: onTabTapped,
-                    items: const [
-                      BottomNavigationBarItem(
-                          icon: Icon(
-                            Icons.home_sharp,
-                            size: 30,
-                          ),
-                          label: "home"),
-                      BottomNavigationBarItem(
-                          icon: Icon(
-                            Icons.qr_code_sharp,
-                            size: 30,
-                          ),
-                          label: "qrcode"),
-                      BottomNavigationBarItem(
-                          icon: Icon(
-                            Icons.airplane_ticket_sharp,
-                            size: 30,
-                          ),
-                          label: "ticket"),
-                      BottomNavigationBarItem(
-                          icon: Icon(
-                            Icons.list,
-                            size: 40,
-                          ),
-                          label: "more")
-                    ],
-                    backgroundColor: Color.fromARGB(255, 0, 166, 232)))));
+        ],
+      ),
+      body: Stack(children: [
+        Container(
+          child: Row(
+            children: widget.filmList,
+          ),
+        )
+      ]),
+      bottomNavigationBar: const CinematixBar(),
+    );
   }
 }
