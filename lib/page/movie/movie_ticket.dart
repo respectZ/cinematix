0ce5eb6a69e1cc25872d30ca7ddad543d99d19f7
Svdex <ucihaibna@yahoo.com>
2022-06-03 07:43:01 +0700

    Update movie ticket

--- lib/page/movie/movie_ticket.dart
+++ lib/page/movie/movie_ticket.dart
@@ -1,11 +1,22 @@
 import 'dart:ffi';
+import 'package:intl/intl.dart';
 
 import 'package:flutter/material.dart';
 import '../../model/cinema_chair.dart';
 import '../../model/cinema_layout.dart';
+import 'package:cinematix/model/ticket.dart';
 
 import 'dart:math';
 import 'dart:developer' as dev;
+
+String _formatRupiah({required int price, bool decimal = false}) {
+  NumberFormat currencyFormatter = NumberFormat.currency(
+    locale: 'id',
+    symbol: 'Rp ',
+    decimalDigits: decimal ? 2 : 0,
+  );
+  return currencyFormatter.format(price);
+}
 
 Widget _buildSchedule() {
   return ListView(
@@ -34,8 +45,8 @@ Widget _buildSchedule() {
 Widget BuildChair(
     {required List<CinemaChair> chair,
     double? width,
-    required List<int> selectedSeatId,
-    required Function(Set<Object>) callback}) {
+    required List<CinemaChair> selectedChair,
+    required Function(CinemaChair) callback}) {
   // Chair.add(CinemaChair(
   //     id: 9999,
   //     row: 15,
@@ -73,13 +84,10 @@ Widget BuildChair(
       width: 40,
       height: 40,
       child: Card(
-        color: selectedSeatId.contains(element.getId())
-            ? Colors.blue
-            : Colors.white,
+        color: selectedChair.contains(element) ? Colors.blue : Colors.white,
         child: InkWell(
           onTap: () {
-            print(element.getId());
-            callback({element.getId(), element.getCode()});
+            callback(element);
           },
           splashColor: Colors.blue.withAlpha(30),
           child: Center(child: Text(element.getCode())),
@@ -114,8 +122,7 @@ class MovieTicketPage extends StatefulWidget {
 }
 
 class _MovieTicketPageState extends State<MovieTicketPage> {
-  List<int> selectedSeatId = [];
-  List<int> selectedSeatCode = [];
+  List<CinemaChair> selectedChair = [];
 
   List<CinemaChair> Chair = List<CinemaChair>.generate(100, (index) {
     var _tempCol = index % 10;
@@ -127,6 +134,9 @@ class _MovieTicketPageState extends State<MovieTicketPage> {
         rightMargin: 0,
         bottomMargin: 0,
         code: "${String.fromCharCode(_tempRow + 65)}${_tempCol}");
+  });
+  List<Ticket> Tickets = List<Ticket>.generate(100, (index) {
+    return Ticket(id: index, cinemaChairId: index, price: 25000);
   });
 
   @override
@@ -159,40 +169,58 @@ class _MovieTicketPageState extends State<MovieTicketPage> {
           ),
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            margin: EdgeInsets.fromLTRB(0.0, 12.0, 0.0, 0.0),
-            child: Text("Pilih Kursi"),
-          ),
-          Container(
-            margin: EdgeInsets.all(12.0),
-            height: layoutHeight,
-            child: BuildChair(
-                chair: Chair,
-                width: screenWidth,
-                selectedSeatId: selectedSeatId,
-                callback: ((Set<Object> res) {
-                  final id = res.elementAt(0) as int;
-                  final code = res.elementAt(1) as String;
-                  setState(() {
-                    if (selectedSeatId.contains(id))
-                      selectedSeatId.remove(id);
-                    else
-                      selectedSeatId.add(id);
-                  });
-                  print(selectedSeatId);
-                })),
-          ),
-          Text("_" * 15),
-          SizedBox(
-            height: 15,
-          ),
-          Text(selectedSeatId.join(" "))
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.fromLTRB(0.0, 12.0, 0.0, 0.0),
+              child: Text("Pilih Kursi"),
+            ),
+            Container(
+                margin: EdgeInsets.all(12.0),
+                height: layoutHeight,
+                child: BuildChair(
+                    chair: Chair,
+                    width: screenWidth,
+                    selectedChair: selectedChair,
+                    callback: (CinemaChair _chair) {
+                      setState(() {
+                        selectedChair.contains(_chair)
+                            ? selectedChair.remove(_chair)
+                            : selectedChair.add(_chair);
+                      });
+                    })),
+            Text("_" * 15),
+            SizedBox(
+              height: 15,
+            ),
+            Text("${selectedChair.map((e) => e.getCode()).join(' ')}"),
+            SizedBox(
+              height: 15,
+            ),
+            Text(_formatRupiah(
+                price: Tickets.map((e) {
+              return selectedChair.map((e2) => e2.getId()).contains(e.getId())
+                  ? e.getPrice()
+                  : 0;
+            }).toList().reduce((value, element) => value + element))),
+            SizedBox(
+              height: 15,
+            )
+          ],
+        ),
       ),
+      bottomNavigationBar: Container(
+          height: 45,
+          child: ElevatedButton(
+            style: selectedChair.length == 0
+                ? ElevatedButton.styleFrom(primary: Colors.grey)
+                : ElevatedButton.styleFrom(),
+            onPressed: (() {}),
+            child: Text("Beli Tiket"),
+          )),
     );
   }
 }
