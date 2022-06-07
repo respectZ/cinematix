7ecc9b587b34a9e83565fbb920fc00ebf46f6e32
ria0901 <80446413+ria0901@users.noreply.github.com>
2022-06-07 11:23:09 +0700

    Add files via upload

---
+++ lib/page/favorit_saya/user_favorite_firebase.dart
@@ -0,0 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseServices {
+  static CollectionReference userfavoriteCollection = FirebaseFirestore.instance.collection('user_favorite');
+
+  static Future<void> AddUserFavorite({String? user_email, int? movie_id})
+async {
+  await FirebaseFirestore.instance
+    .collection('user_favorite')
+    .add({'user_email' : 'ria@gmail.com', 'movie_id' : 1});
+  }
+
+  static Future<void> UpdateUserFavorite({String? user_email, int? movie_id}) async{
+    await FirebaseFirestore.instance.collection('user_favorite').doc(user_email).set({
+      'user_email' : user_email,
+      'movie_id' : movie_id
+    });
+  }
+}
