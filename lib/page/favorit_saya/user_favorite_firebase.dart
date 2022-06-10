e1a963cea43ec340a57b478592bc2e9314686dfa
ria0901 <80446413+ria0901@users.noreply.github.com>
2022-06-10 23:25:26 +0700

    Update user_favorite_firebase.dart
    add status

--- lib/page/favorit_saya/user_favorite_firebase.dart
+++ lib/page/favorit_saya/user_favorite_firebase.dart
@@ -3,11 +3,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class DatabaseServices {
   static CollectionReference userfavoriteCollection = FirebaseFirestore.instance.collection('user_favorite');
 
-  static Future<void> AddUserFavorite({String? user_email, int? movie_id})
+  static Future<void> AddUserFavorite({String? user_email, int? movie_id, String? status})
 async {
   await FirebaseFirestore.instance
     .collection('user_favorite')
-    .add({'user_email' : 'ria@gmail.com', 'movie_id' : 1});
+    .add({'user_email' : 'ria@gmail.com', 
+          'movie_id' : 1,
+         'status' : 'Kamu sudah memberikan review'});
   }
 
   static Future<void> UpdateUserFavorite({String? user_email, int? movie_id}) async{
